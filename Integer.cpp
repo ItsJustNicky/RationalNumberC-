c6d9ecaa64432b99b759711c0e2b249107b6424a
Daniel Prvanov <prvda311@student.otago.ac.nz>
2023-05-23 13:19:20 +1200

    We started on the operators

--- Integer.cpp
+++ Integer.cpp
@@ -12,17 +12,24 @@ namespace cosc326 {
 		value = i.value;
 	}
 
+	/* Takes in a string and passes it to int */
 	Integer::Integer(const std::string& s) {
 		if (s.empty()) {
 			value = 0;
 		} else {
 			value = std::stoi(s);
+			if (value > 0) {
+				posOrNeg = 1;
+			} else if (value < 0) {
+				posOrNeg = 2;
+			} else if (value == 0) {
+				posOrNeg = 0;
+			}
 		}
 	}
 
-
 	Integer::~Integer() {
-	
+		value = 0;
 	}
 
 	Integer& Integer::operator=(const Integer& i) {
@@ -30,6 +37,10 @@ namespace cosc326 {
 	}
 
 	Integer Integer::operator-() const {
+		Integer clone = Integer(*this);
+		*this - clone;
+		*this - clone;
+
 		return Integer(*this);
 	}
 
@@ -38,32 +49,42 @@ namespace cosc326 {
 	}
 
 	Integer& Integer::operator+=(const Integer& i) {
+		*this + Integer(i);
 		return *this;
 	}
 
 	Integer& Integer::operator-=(const Integer& i) {
+		*this - Integer(i);
 		return *this;
 	}
 
 	Integer& Integer::operator*=(const Integer& i) {
+		*this * Integer(i);
 		return *this;
 	}
 
 	Integer& Integer::operator/=(const Integer& i) {
+		*this / Integer(i);
 		return *this;
 	}
 
 	Integer& Integer::operator%=(const Integer& i) {
+		*this % Integer(i);
 		return *this;
 	}
 
 	Integer operator+(const Integer& lhs, const Integer& rhs) {
-		return lhs;
+		Integer out;
+		out.operator+=(rhs);
+		out.operator+=(lhs);
+		return out;
 	}
 
 	Integer operator-(const Integer& lhs, const Integer& rhs) {
-		return lhs;
-	}
+		Integer out;
+		out.operator-=(rhs);
+		out.operator-=(lhs);
+		return out;	}
 
 	Integer operator*(const Integer& lhs, const Integer& rhs) {
 		return lhs;
