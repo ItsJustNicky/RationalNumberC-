f549dfbd4556d75a48f089c81e14a4cdd840664a
patni711 <patni711@student.otago.ac.nz>
2023-06-12 16:09:25 +1200

    Replace main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 */
 
 #include "Integer.h"
-#include "Rational.h"
+//#include "Rational.h"
 #include <iostream>
 using namespace std;
 using namespace cosc326;
@@ -14,12 +14,18 @@ int main() {
 
 	std::cout << "Kia ora World" << std::endl;
 	cosc326::Integer myInteger;
-	Integer a = Integer("-20");
+	Integer a = Integer("-1");
 	std::cout << a << std::endl;
-	Integer b = Integer("10");
+	Integer b = Integer("3");
 	std::cout << b << std::endl;
-	Integer sum = a + b;
+	
+	//Integer sum2 = sum * a;
+	//if (a <= b)std::cout << a << std::endl;
+	//a /= b;
+	//std::cout << a << std::endl;
+	Integer sum = a*b;
 	std::cout << sum << std::endl;
+	//std::cout << sum2 << std::endl;
 	//bool hmm = a == b;
 	//std::cout << "----" << std::endl;
 	//std::cout << hmm << std::endl;
