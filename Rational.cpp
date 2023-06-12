cd8de2f0f9979860a225ea99f5adfa1b2eda4132
patni711 <patni711@student.otago.ac.nz>
2023-06-12 16:08:25 +1200

    Replace Rational.cpp

--- Rational.cpp
+++ Rational.cpp
@@ -3,31 +3,89 @@
 namespace cosc326 {
 
 	Rational::Rational() {
-		value = 0;
+		posOrNeg = true;
+		value.push_back(0);
+		dec.push_back(0);
 	}
 
-	Rational::Rational(const std::string& str) {
+	Rational::Rational(const std::string& s) {
+		std::string temp;
+		bool isDec = false;
 		if (s.empty()) {
-			value = 0;
-		} else {
-			value = std::stoi(s);
+			posOrNeg = true;
+			value.push_back(0);
+		}
+		else {
+			if (s[0] == '-') {
+				posOrNeg = false;
+				temp = s.substr(1);
+			}
+			else if (s[0] == '+') {
+				posOrNeg = true;
+				temp = s.substr(1);
+			}
+			else {
+				temp = s;
+				posOrNeg = true;
+			}
+			for (char c : temp) {
+				if (c == '.') {
+					isDec = true;
+				}
+				if (std::isdigit(c)&&!isDec) {
+					value.push_back(c - '0');
+				}if (std::isdigit(c)&&isDec) {
+					dec.push_back(c - '0');
+				}
+				else {
+					// Handle error: invalid input string
+					value.clear();
+					dec.clear();
+					posOrNeg = true;
+					value.push_back(0);
+					dec.push_back(0);
+					break;
+				}
+			}
 		}
 	}
 
 	Rational::Rational(const Rational& r) {
-
+		value = r.value;
 	}
 
 	Rational::Rational(const Integer& a) {
-
+		value = a.getValue();
 	}
 
 	Rational::Rational(const Integer& a, const Integer& b) {
-
+		Integer top = Integer(a);
+		top /= b;
+		Integer bottom = Integer(a);
+		bottom %= b;
+		value = top.getValue();
+		Integer bClone = b;
+		int decConversion=bottom.concatenateIntegers(bottom.getValue());
+		int bConversion = bClone.concatenateIntegers(bClone.getValue());
+		decConversion = decConversion / bConversion;
+		std::string strNumber = std::to_string(decConversion);
+		Integer out = (strNumber);
+		dec = out.getValue();
 	}
 
 	Rational::Rational(const Integer& a, const Integer& b, const Integer& c) {
-
+		Integer top = Integer(b);
+		top /= c;
+		Integer bottom = Integer(b);
+		bottom %= c;
+		value = a.getValue();
+		Integer cClone = c;
+		int decConversion = bottom.concatenateIntegers(bottom.getValue());
+		int bConversion = cClone.concatenateIntegers(cClone.getValue());
+		decConversion = decConversion / bConversion;
+		std::string strNumber = std::to_string(decConversion);
+		Integer out = (strNumber);
+		dec = out.getValue();
 	}
 
 	Rational::~Rational() {
@@ -35,11 +93,16 @@ namespace cosc326 {
 	}
 
 	Rational& Rational::operator=(const Rational& r) {
+		posOrNeg = r.posOrNeg;
+		value.clear();
+		value = r.value;
+		dec = r.dec;
 		return *this;
 	}
 
 	Rational Rational::operator-() const {
-		return Rational(*this);
+		
+		return *this;
 	}
 
 	Rational Rational::operator+() const {
